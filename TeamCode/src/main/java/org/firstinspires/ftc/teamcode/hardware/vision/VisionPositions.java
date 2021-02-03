85eaa03b133de4855d5df76e1c76e5d0d0db4493
Mich-T <mitoryu@students.natickps.org>
2021-02-03 17:17:41 -0500

    realized we have blue field, changed values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/vision/VisionPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/vision/VisionPositions.java
@@ -12,13 +12,13 @@ public class VisionPositions {
     //The grid goes from x = -24 to x = 72 and y = -72 to y = 72
     //Remember to flip x and y and take the inverse of the x
     public VisionPositions(){
-        leftGoal = new VisionPos(24,-48, "Left WobbleGoal");
-        rightGoal = new VisionPos(48, -48, "Right WobbleGoal");
-        initstack = new VisionPos(36, -36, "Stack");
+        leftGoal = new VisionPos(-2*SQUARE,-2*SQUARE, "Left WobbleGoal");
+        rightGoal = new VisionPos(-SQUARE, -2*SQUARE, "Right WobbleGoal");
+        initstack = new VisionPos(-36, -36, "Stack");
 
-        targetA = new VisionZone(48, 72, 0, 24, "Zone A");
-        targetB = new VisionZone(24, 48, 24, 48, "Zone B");
-        targetC = new VisionZone(48, 72, 48, 72, "Zone C");
-        launchLine = new VisionZone(0, 72, 12, 12, "Launch Line");
+        targetA = new VisionZone(-48, -72, 0, 24, "Zone A");
+        targetB = new VisionZone(-24, -48, 24, 48, "Zone B");
+        targetC = new VisionZone(-48, -72, 48, 72, "Zone C");
+        launchLine = new VisionZone(0, -72, 12, 12, "Launch Line");
     }
 }
